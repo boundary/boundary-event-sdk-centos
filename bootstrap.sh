62e10421adbb7030c429216c87df5aff874caf25
jdgwartney <davidg@boundary.com>
2014-04-23 13:19:36 -0700

    First steps in getting Boundary SDK installed automatically on CentOS 6.5

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash 
 
 #
-# Create a path to a log file that encodes
-# into the path the date and time to minute resolution
+# Setup variable to track installation logging
 #
-LOG="$PWD/log.$(date +"%Y-%m-%dT%H:%m")"
+LOG="$PWD/install.log"
 
-# To handle global redirection
-#exec 3>&1 4>&2 >>$LOG 2>&1
-#exec 1>&3 2>&4
+# Explicitly set the HOME variable
+export HOME=~vagrant
 
 log() {
   typeset -r msg=$1
@@ -19,15 +17,23 @@ log() {
 # Update packages 
 #
 log "Updating packages..."
-sudo yum update >> $LOG 2>&1
+
+#
+# Create standard directories
+#
+DOWNLOADS_DIR=/downloads
+TOOLS_DIR=${HOME}/tools
+mkdir -p ${DOWNLOADS_DIR}
+# Create a directory to install all local non-RPM distributions
+mkdir -p ${TOOLS_DIR}
 
 #
 # Packages for sane administration
 #
-log "Installing system adminstration packages..."
+log "Install system adminstration packages..."
 sudo yum install -y man wget >> $LOG 2>&1
 
-log "Installing EPEL gpg keys and package..."
+log "Install EPEL gpg keys and package..."
 wget https://fedoraproject.org/static/0608B895.txt >> $LOG 2>&1
 sudo mv 0608B895.txt /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 >> $LOG 2>&1
 sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 >> $LOG 2>&1
@@ -46,11 +52,62 @@ log "Install required packages for RVM..."
 sudo yum install -y patch libyaml-devel libffi-devel autoconf gcc-c++ patch readline-devel openssl-devel automake libtool bison >> $LOG 2>&1
 
 #
-# Required packages for Boundary Event Integration
+# Required packages for Boundary Event Plugins
 #
 log "Install required packages for Boundary Event Integration..."
 sudo yum install -y curl unzip >> $LOG 2>&1
 
+#
+# Required packages for Boundary Event SDK
+#
+log "Install required packages for Boundary Event SDK..."
+sudo yum install -y java-1.7.0-openjdk git >> $LOG 2>&1
+
+# Add Java bin directory in the path
+echo "" >> ${HOME}/.bash_profile
+echo '# java configuration' >> ${HOME}/.bash_profile
+echo "JAVA_HOME="/usr/lib/jvm/jre-1.7.0-openjdk.x86_64 >> ${HOME}/.bash_profile
+echo 'export JAVA_HOME ' >> ${HOME}/.bash_profile
+echo "" >> ${HOME}/.bash_profile
+
+log "Install maven for Boundary Event SDK..."
+
+MAVEN_DIR=apache-maven-3.2.1
+MAVEN_TAR=${MAVEN_DIR}-bin.tar.gz
+MAVEN_URI=http://apache.cs.utah.edu/maven/maven-3/3.2.1/binaries/${MAVEN_TAR}
+
+# Fetch the distribution
+pushd ${DOWNLOADS_DIR} > /dev/null 2>&1
+wget ${MAVEN_URI} >> $LOG 2>&1
+popd > /dev/null 2>&1
+
+pushd ${TOOLS_DIR} > /dev/null 2>&1
+tar xvf ${DOWNLOADS_DIR}/${MAVEN_TAR} >> $LOG 2>&1
+popd > /dev/null 2>&1
+
+# Add Maven bin directory in the path
+echo "# Add maven to path" >> ${HOME}/.bash_profile
+echo "MAVEN_INSTALL="${TOOLS_DIR}/${MAVEN_DIR} >> ${HOME}/.bash_profile
+echo 'export PATH=$PATH:$MAVEN_INSTALL/bin' >> ${HOME}/.bash_profile
+echo "" >> ${HOME}/.bash_profile
+
+# Install the Boundary Event SDK
+log "Install Boundary Event SDK..."
+SDK_LOG="$PWD/boundary_sdk_log.$(date +"%Y-%m-%dT%H:%m")"
+source $HOME/.bash_profile
+
+git clone https://github.com/boundary/boundary-event-sdk.git > $SDK_LOG 2>&1
+pushd boundary-event-sdk >> $SDK_LOG 2>&1
+bootstrap.sh >> $SDK_LOG 2>&1
+bash bootstrap.sh  >> $SDK_LOG 2>&1
+source bsdk-env.sh  >> $SDK_LOG 2>&1
+mvn install >> $SDK_LOG 2>&1
+popd  >> $SDK_LOG 2>&1
+
+NAGIOS_FILE=$HOME/nagios
+
+if [ -r ${NAGIOS_FILE} ]
+then
 #
 # Add the nagios user and groups
 #
@@ -140,6 +197,7 @@ sudo chkconfig --add nagios >> $LOG 2>&1
 sudo chkconfig --level 35 nagios on >> $LOG 2>&1
 sudo chkconfig --add httpd >> $LOG 2>&1
 sudo chkconfig --level 35 httpd on >> $LOG 2>&1
+fi
 
 log "Details of the installation have been logged to $LOG"
 
